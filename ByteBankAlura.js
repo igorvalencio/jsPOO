24c9f03d53c3b01180202ac94ce4f6d97b2a2de3
Igor Neves <igornevesvalencio@hotmail.com>
2023-04-25 12:56:07 -0300

    return;;;;

--- ByteBankAlura.js
+++ ByteBankAlura.js
@@ -7,19 +7,20 @@ class cliente{
 class contacorrente
 {
     agencia;
+    //# significa que é privado
+    //quando tem _ antes, significa que é privado também.
      #saldo = 0;
     sacar(valor){
         if(this.#saldo >= valor){
         this.#saldo -= valor;
+        return valor;
         }else{
             console.log("Não dá pra ficar negativo irmão")
         }
     }
     depositar(valor){
-    if(valor > 0)
-    {
-        this.#saldo += valor;
-    }
+    if(valor <=0) return console.log("Tá de sacanagem");
+    this.#saldo += valor;
     }
 }
 
@@ -33,8 +34,10 @@ cliente2.cpf = 32165498700
 
 const contacorrenteJonatas = new contacorrente();
 contacorrenteJonatas.agencia = 1001;
-contacorrenteJonatas.depositar(100);
-contacorrenteJonatas.sacar(20);
+contacorrenteJonatas.depositar(0);
+
+const valorSacado = contacorrenteJonatas.sacar(20);
+
 
 
 
